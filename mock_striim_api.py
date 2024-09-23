da3c8f66035d79a266dedf9c1b99f02b6942d582
Benny Tavarez <bennytavarez@hotmail.com>
2024-09-23 09:52:31 -0400

    add code to remove alerts after 3 seconds

--- mock_striim_api.py
+++ mock_striim_api.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, redirect, jsonify, request
 import os 
 import requests
+import json
 
 app = Flask(__name__)
 
@@ -30,17 +31,34 @@ def get_status():
 @app.route('/api/v1/server/commands', methods=['GET'])
 def show_commands():
 
-    return jsonify({
-        "status": striim_status["status"]
+     return jsonify({
+        "status": "Status: " + striim_status["status"]
     })
 
 
 
 @app.route('/api/v1/server/restart', methods=['POST'])
 def restart_server():
-    if striim_status["status"] == "running":
+
+    data = request.get_json()
+    command = data.get('command')
+    ship = data.get('ship')
+
+    print(f"Received data: {data}")
+
+    print(f"Received ship: {ship}")
+
+    print(f"Received command: {command}")
+
+    # return jsonify({"status":"success", "command": command})
+
+    # return jsonify({"commands": json.loads(response.text)})
+
+    if striim_status["status"] in ["running", "stopped"]:
         striim_status["status"] = "restarting"
         return jsonify({"message": "Server is restarting", "status":"restarting"}), 200
+    elif striim_status["status"] == 'restarting':
+        return jsonify({"message": "Server is already restarting, please wait."})
     else:
         return jsonify({"message":"Server is not running, cannot restart", "status": striim_status["status"]}), 400
     
@@ -50,8 +68,14 @@ def stop_server():
     if striim_status["status"] == "running":
         striim_status["status"] = "stopped"
         return jsonify({"message": "Server is stopping", "status":"stopped"}), 200
+    elif striim_status["status"] == "restarting":
+        striim_status["status"] = "stopped"
+        return jsonify({"message": "Server is stopping", "status":"stopped"}), 200
+    elif striim_status["status"] == "stopped":
+        striim_status["status"] = "stopped"
+        return jsonify({"message": "Server is not running, cannot stopped", "status":"stopped"}), 200
     else:
-        return jsonify({"message":"Server is not running, cannot stop", "status": striim_status["status"]}), 400
+        return jsonify({"message":f"Server is in an unknown state: {striim_status['status']}, cannot stop", "status": striim_status["status"]}), 400
 
 @app.route('/api/v1/server/start', methods=['POST'])
 def start_server():
@@ -59,8 +83,14 @@ def start_server():
     if striim_status["status"] == "stopped":
         striim_status["status"] = "running"
         return jsonify({"message": "Server is starting", "status":"running"}), 200
+    elif striim_status["status"] == "restarting":
+        striim_status["status"] = "running"
+        return jsonify({"message": "Server is starting", "status":"running"}), 200
+    elif striim_status["status"] == "running":
+        striim_status["status"] = "running"
+        return jsonify({"message": "Server is is already running", "status":"running"}), 200
     else:
-        return jsonify({"message":"Server is already running", "status": striim_status["status"]}), 400
+        return jsonify({"message":f"Server is in an unknown state: {striim_status['status']}, cannot start", "status": striim_status["status"]}), 400
         
 
 if __name__ == '__main__':
--- ship_app.py
+++ ship_app.py
@@ -1,42 +1,142 @@
 from flask import Flask, render_template, jsonify, request
 from requests.exceptions import RequestException
 import requests
+import json
 
 app = Flask(__name__)
 
 striim_api_url = "http://localhost:5001/api/v1"
+auth_path = '/security/authenticate'
 
-SHIP_API_MAP = {"Ship 1":"http://ship1", "Ship 2":"http://localhost:5001/api/v1", "Ship 3":"http://ship3", "Ship 4":"http://ship4", "Ship 5":"http://ship5"
+SHIP_API_MAP = {"Ship 1":"http://localhost:5001/api/v1", "Ship 2":"http://localhost:5001/api/v1", "Ship 3":"http://localhost:5001/api/v1", 
+                "Ship 4":"http://localhost:5001/api/v1", "Ship 5":"http://localhost:5001/api/v1"
                 }
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+# def get_token(username, password):
+#         payload = {'username': username, 'password': password}
+
+#         try:
+#             url = striim_api_url + auth_path
+#             resp = requests.post(url, data=payload)
+#             if resp.status_code != 200:
+#                 return "Likely: wrong username / password"
+#             tokenjson = json.loads(resp.text)
+#             return tokenjson["token"]
+#         except RequestException as ex:
+#             raise ex
+        
+# def health_stats(authToken):
+#     resp = requests.get(f'http://{striim_api_url}/health?token={authToken}')
+#     data = json.loads(resp.text)
+#     print(data)
+#     return data
+
+
 @app.route('/striim/sendcommands', methods=['POST'])
 def send_commands_to_ship():
 
-    ship = request.form.get('ship').capitalize()
-    commands = request.form.get('commands')
+    data = request.get_json()
+    selected_ship = data.get('ship')
+    command = data.get('command')
 
-    api_url = SHIP_API_MAP.get('ship')
-    
-    if not commands or not ship:
+    ship_url = SHIP_API_MAP.get(selected_ship)
+    print(SHIP_API_MAP[selected_ship] )
+
+
+    print(f"This is the ship chosen: {selected_ship}")
+    print(f"This command was received:{command}")
+
+    if not command or not selected_ship:
         return jsonify({"error": "Commands and ship(s) must be provided"}), 400
 
-    print(f"This is the ship chosen: {ship}")
-    print(f"This command was received:{commands}")
+    if not ship_url:
+        return jsonify({"error": f"Could not find {selected_ship}"}), 404
+    
+    payload = {
+        "ship": selected_ship, 
+        "command": command}
+    
+    print(f"PAYLOAD: {payload}")
 
+    
     try:
-        response = requests.post(f'{api_url}/server/commands', json={"commands": commands, "ship": ship})
+        response = requests.post(f'{ship_url}/server/restart', json=payload)
         if response.status_code == 200:
-            return jsonify(response.json())
+            return jsonify(response.json()), 200
         else:
+            print(f"failed with statuscode {response.status_code}")
             return jsonify({"error": "Failed to send command"}), response.status_code
-
     except requests.RequestException as e:
+        print(f'RequestException: {str(e)}')
         return jsonify({"error": str(e)}), 500
 
 
+@app.route('/striim/stop', methods=['POST'])
+def stop_server():
+    
+    selected_ship = request.form.get('ship')
+    print(SHIP_API_MAP[selected_ship] )
+
+
+    print(f"Selected ship {selected_ship}")
+    print(f"Stopping ship {selected_ship}")
+
+    if not selected_ship:
+        return jsonify({"error": "please select a ship"}), 400
+    
+    ship_url = SHIP_API_MAP.get(selected_ship)
+
+    if not ship_url:
+        return jsonify({"error": f"Could not find {selected_ship}"}), 404
+
+    try:
+        response = requests.post(f'{ship_url}/server/stop')
+        if response.status_code == 200:
+            return jsonify({"message":f"Server on {selected_ship} is stopping", "status":"stopped"}), 200
+        else:
+            return jsonify({"error": f"Failed to stop {selected_ship}"}), response.status_code
+    except requests.RequestException as e:
+        return jsonify({"error": f"{e}"}), 500
+        
+@app.route('/striim/start', methods=['POST'])
+def start_server():
+
+    selected_ship = request.form.get('ship')
+    print(SHIP_API_MAP[selected_ship] )
+
+    print(f"Selected ship {selected_ship}")
+    print(f"Starting ship {selected_ship}")
+
+    if not selected_ship:
+        return jsonify({"error": "please select a ship"}), 400
+    
+    ship_url = SHIP_API_MAP.get(selected_ship)
+
+    if not ship_url:
+        return jsonify({"error": f"Could not find {selected_ship}"}), 404
+
+    try:
+        response = requests.post(f'{ship_url}/server/start')
+        if response.status_code == 200:
+            return jsonify({"message":f"Server on {selected_ship} is starting", "status":"running"}), 200
+        else:
+            return jsonify({"error": f"Failed to start {selected_ship}"}), response.status_code
+    except requests.RequestException as e:
+        return jsonify({"error": f"{e}"}), 500
+
+
+@app.after_request    
+def add_header(response):
+    response.headers['Cache-Control'] ='no-store, no-chache, must-revalidate, post-check=0, pre-check=0, max-age=0'
+    response.headers['Pragma'] = 'no-cache'
+    response.headers['Expires'] = '-1'
+    return response
+
 if __name__ == '__main__':
+    # auth_token = get_token('admin', 'admin')
+    # health_stats(auth_token)
     app.run(debug=True, port=5000)
